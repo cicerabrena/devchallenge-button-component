feccae9a46aeedb332890d89b46e4822201b4a1b
Cicera Brena <cicerabrens@gmail.com>
2022-03-01 02:56:00 -0300

    :recycle: Turn button props into to small functions

--- src/components/Button/Button.style.js
+++ src/components/Button/Button.style.js
@@ -2,9 +2,113 @@ import styled, { css } from 'styled-components';
 
 const Style = {};
 
+function getSize(size) {
+	const sizes = {
+		sm: () => `
+			padding: 0.3rem 0.7rem;
+			height: 32px;
+		`,
+		md: () => `
+			padding: 0.5rem 1rem;
+			height: 36px;
+		`,
+		lg: () =>
+			`
+				padding: 0.7rem 1.3rem;
+				height: 42px;
+			`,
+	};
+
+	return sizes[size || 'md'];
+}
+
+function getBackgroundColor(color = 'default', disableShadow = false) {
+	const backgrounds = {
+		default: () => `
+			background-color: #e0e0e0;
+			color: #3f3f3f;
+			box-shadow: ${disableShadow ? 'none' : '0px 2px 3px 0px rgba(51, 51, 51, 0.2)'};
+
+			&:hover,
+			&:focus {
+				background-color: rgba(174, 174, 174, 1);
+			}
+		`,
+		primary: () => `
+			background-color: #2962ff;
+			color: white;
+			box-shadow: 0px 2px 3px 0px rgba(41, 98, 255, 0.2);
+
+			&:hover,
+			:focus {
+				background-color: #0039cb;
+			}
+		`,
+		secondary: () => `
+			background-color: #455a64;
+			color: white;
+			box-shadow: 0px 2px 3px 0px rgba(69, 90, 100, 0.2);
+
+			&:hover,
+			&:focus {
+				background-color: #1c313a;
+			}
+		`,
+		danger: () => `
+			background-color: #d32f2f;
+			color: white;
+			box-shadow: 0px 2px 3px 0px rgba(211, 47, 47, 0.2);
+
+			&:hover,
+			&:focus {
+				background-color: #9a0007;
+			}
+		`,
+	};
+
+	return backgrounds[color || 'default']();
+}
+
+function getVariant({ variant, color, disableShadow, size }) {
+	const styles = {
+		text: () => `
+			background-color: transparent;
+			color: #3d5afe;
+
+			&:hover,
+			:focus {
+				background-color: rgba(41, 98, 255, 0.1);
+			}
+
+			&:disabled {
+				color: #9e9e9e;
+			}
+		`,
+		outline: () =>
+			`
+				background-color: transparent;
+				border: 1px solid #3d5afe;
+				color: #3d5afe;
+
+				&:hover,
+				:focus {
+					background-color: rgba(41, 98, 255, 0.1);
+				}
+			`,
+		default: () => getBackgroundColor(color, disableShadow),
+	};
+
+	const style = css`
+		${styles[variant || 'default']}${getSize(size)}
+	`;
+
+	return style;
+}
+
 Style.Button = styled.button`
 	display: inline-flex;
 	align-items: center;
+	justify-content: center;
 	border: none;
 	border-radius: 5px;
 	font-family: 'Noto Sans JP', sans-serif;
@@ -18,123 +122,7 @@ Style.Button = styled.button`
 		pointer-events: none;
 	}
 
-	${(props) => {
-		switch (props.variant) {
-			case 'text':
-				return css`
-					background-color: transparent;
-					color: #3d5afe;
-
-					&:hover,
-					:focus {
-						background-color: rgba(41, 98, 255, 0.1);
-					}
-
-					&:disabled {
-						color: #9e9e9e;
-					}
-				`;
-			case 'outline':
-				return css`
-					background-color: transparent;
-					border: 1px solid #3d5afe;
-					color: #3d5afe;
-
-					&:hover,
-					:focus {
-						background-color: rgba(41, 98, 255, 0.1);
-					}
-				`;
-			default:
-				return css`
-					background-color: #e0e0e0;
-					box-shadow: ${props.disableShadow
-						? 'none'
-						: '0px 2px 3px 0px rgba(0, 49, 202, 0.2)'};
-
-					&:hover,
-					&:focus {
-						background-color: #aeaeae;
-					}
-
-					&:disabled {
-						background-color: #e0e0e0;
-						font-weight: bold;
-						color: #9e9e9e;
-					}
-
-					${(prev) => {
-						switch (prev.color) {
-							case 'default':
-								return css`
-									background-color: #e0e0e0;
-									color: #3f3f3f;
-									box-shadow: 0px 2px 3px 0px rgba(51, 51, 51, 0, 2);
-
-									&:hover,
-									&:focus {
-										background-color: rgba(174, 174, 174, 1);
-									}
-								`;
-							case 'primary':
-								return css`
-									background-color: #2962ff;
-									color: white;
-									box-shadow: 0px 2px 3px 0px rgba(41, 98, 255, 0, 2);
-
-									&:hover,
-									:focus {
-										background-color: #0039cb;
-									}
-								`;
-							case 'secondary':
-								return css`
-									background-color: #455a64;
-									color: white;
-									box-shadow: 0px 2px 3px 0px rgba(69, 90, 100, 0, 2);
-
-									&:hover,
-									&:focus {
-										background-color: #1c313a;
-									}
-								`;
-							case 'danger':
-								return css`
-									background-color: #d32f2f;
-									color: white;
-									box-shadow: 0px 2px 3px 0px rgba(211, 47, 47, 0, 2);
-
-									&:hover,
-									&:focus {
-										background-color: #9a0007;
-									}
-								`;
-							default:
-								return css`
-									background-color: #e0e0e0;
-								`;
-						}
-					}}
-				`;
-		}
-	}}
-
-	${(props) => {
-		switch (props.size) {
-			case 'sm':
-				return css`
-					padding: 0.3rem 0.7rem;
-				`;
-			case 'lg':
-				return css`
-					padding: 0.7rem 1.3rem;
-				`;
-			default:
-				return css`
-					padding: 0.5rem 1rem;
-				`;
-		}
-	}}
+	${(props) => getVariant({ ...props })}
 
 	span {
 		&.start {
